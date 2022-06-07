1660dce7cb1f0eed0f842e93e40caec54c00f361
DelphinePythonique <delphine.lemire+openclassroom@gmail.com>
2022-06-07 11:50:17 +0200

    v0.0.2 optimised file

--- optimized.py
+++ optimized.py
@@ -16,13 +16,19 @@ def measure(func):
 
     return _time_it
 
+def profit_to_percent(cost,profit):
+    if cost == 0:
+        return 0
+    else:
+        return (100*profit/cost)
 
 def extract_percent(param):
     return float(param.split("%")[0])
 
 
 class Equity:
-    def __init__(self, name: "string", cost: "float", percent_profit: "float"):
+    def __init__(self, name: "string", cost: "float", percent_profit: "float", id: int):
+        self.id = id
         self.name = name
         self.cost = cost
         self.percent_profit = percent_profit
@@ -39,52 +45,7 @@ class Repository:
     def __init__(self):
         self.equities = []
 
-    def _number_of_equities(self):
-        return len(self.equities)
-
-    def add_equity(self, equity: "Equity"):
-        self.equities.append(equity)
-
-    def get_equities_sorted_by_percent_cost(self):
-        return sorted(self.equities, key=lambda equity: equity.percent_profit, reverse=True)
-
-    @measure
-    def get_best_combination(self, max_investment):
-        total_profit = 0
-        total_cost = 0
-        combination = []
-        equities = []
-        i = 0
-        equities_sorted = self.get_equities_sorted_by_percent_cost()
-        """
-         {
-                    "combination": combination,
-                    "equities": self._get_equities_from_index_tuple(combination),
-                    "cost": self._get_sum_cost_from_index_tuple(combination),
-                    "profit": self._get_sum_profit_from_index_tuple(combination),
-                }
-        """
-        while i < len(equities_sorted) :
-            new_total_cost = total_cost + equities_sorted[i].cost
-            if new_total_cost <= max_investment:
-                total_cost = new_total_cost
-                total_profit += equities_sorted[i].profit
-                combination.append(i)
-                equities.append(equities_sorted[i])
-            i += 1
-
-        return {"combination": tuple(combination),
-                "equities": tuple(equities),
-                "cost": total_cost,
-                "profit": total_profit}
-
-
-class App:
-
-    def __init__(self):
-        self.actions_repository = Repository()
-
-    def populate_equities_with_csv(self, csv_enquiries):
+    def populate_equities_with_csv_new(self, csv_enquiries):
         with open(csv_enquiries, newline="") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
@@ -93,37 +54,91 @@ class App:
                         row["action"],
                         float(row["cost"]),
                         extract_percent(row["percent"]),
+                        len(self.equities)
                     )
                 )
 
-    def populate_equities(self):
-        self.actions_repository.add_equity(Equity("Action-1", 20, 5))
-        self.actions_repository.add_equity(Equity("Action-2", 30, 10))
-        self.actions_repository.add_equity(Equity("Action-3", 50, 15))
-        self.actions_repository.add_equity(Equity("Action-4", 70, 20))
-        self.actions_repository.add_equity(Equity("Action-5", 60, 17))
+    def populate_equities_with_csv_old(self, csv_enquiries):
+        with open(csv_enquiries, newline="") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if float(row["price"])>0:
+                    self.add_equity(
+                        Equity(
+                            row["name"],
+                            float(row["price"]),
+                            float(row["profit"]),
+                            len(self.equities)
+                        )
+                    )
 
-        self.actions_repository.add_equity(Equity("Action-6", 80, 25))
-        self.actions_repository.add_equity(Equity("Action-7", 22, 7))
-        self.actions_repository.add_equity(Equity("Action-8", 26, 11))
-        self.actions_repository.add_equity(Equity("Action-9", 48, 13))
-        self.actions_repository.add_equity(Equity("Action-10", 34, 27))
-        self.actions_repository.add_equity(Equity("Action-11", 42, 17))
-        self.actions_repository.add_equity(Equity("Action-12", 110, 9))
-        self.actions_repository.add_equity(Equity("Action-13", 38, 23))
-        self.actions_repository.add_equity(Equity("Action-14", 14, 1))
-        self.actions_repository.add_equity(Equity("Action-15", 18, 3))
-        self.actions_repository.add_equity(Equity("Action-16", 8, 8))
-        self.actions_repository.add_equity(Equity("Action-17", 4, 12))
-        self.actions_repository.add_equity(Equity("Action-18", 10, 14))
-        self.actions_repository.add_equity(Equity("Action-19", 24, 21))
-        self.actions_repository.add_equity(Equity("Action-20", 114, 18))
+    def _number_of_equities(self):
+        return len(self.equities)
 
+    def add_equity(self, equity: "Equity"):
+        self.equities.append(equity)
+
+    def get_equities_sorted(self, key):
+        return sorted(self.equities, key=lambda equity: getattr(equity, key), reverse=True)
+
+    @measure
+    def get_best_combination(self, max_investment, sorted_key):
+        total_profit = 0
+        total_cost = 0
+        combination = []
+        equities = []
+        i = 0
+        equities_sorted = self.get_equities_sorted(sorted_key)
+        """
+         {
+                    "combination": combination,
+                    "equities": self._get_equities_from_index_tuple(combination),
+                    "cost": self._get_sum_cost_from_index_tuple(combination),
+                    "profit": self._get_sum_profit_from_index_tuple(combination),
+                }
+        """
+        while i < len(equities_sorted):
+            new_total_cost = total_cost + equities_sorted[i].cost
+            if new_total_cost <= max_investment:
+                total_cost = new_total_cost
+                total_profit += equities_sorted[i].profit
+                equities.append(equities_sorted[i])
+            i += 1
+
+        return {"equities": tuple(equities),
+                "cost": total_cost,
+                "profit": total_profit}
+
+class App:
+
+    def __init__(self):
+        self.actions_repository = Repository()
 
 if __name__ == "__main__":
     app = App()
-    app.populate_equities_with_csv("dataset1_Python+P7.csv")
-    result = app.actions_repository.get_best_combination(500)
-    print(
-        f"{result['equities']}: for profit of {result['profit']} from cost {result['cost']} "
-    )
+    app.actions_repository.populate_equities_with_csv_old("dataset2_Python+P7.csv")
+    result = app.actions_repository.get_best_combination(500, "percent_profit")
+    print(f"solution by percent_profit for profit of {result['profit']} from cost {result['cost']} ")
+
+    print(f"name, id,cost, .percent_profit,profit")
+    print("\n".join([f"{equities.name}, {equities.id}, {equities.cost}, {equities.percent_profit}, {equities.profit}" for equities in result['equities']]))
+
+
+    result = app.actions_repository.get_best_combination(500, "cost")
+    print(f":solution by cost: for profit of {result['profit']} from cost {result['cost']} ")
+
+    print(f"name, id,cost, .percent_profit,profit")
+    print("\n".join(
+        [f"{equities.name}, {equities.id}, {equities.cost}, {equities.percent_profit}, {equities.profit}" for equities
+         in result['equities']]))
+
+
+    result = app.actions_repository.get_best_combination(500, "profit")
+    print(f": solution by profit for profit of {result['profit']} from cost {result['cost']} ")
+
+
+    print(f"name, id,cost, .percent_profit,profit")
+    print("\n".join(
+        [f"{equities.name}, {equities.id}, {equities.cost}, {equities.percent_profit}, {equities.profit}" for equities
+         in result['equities']]))
+
